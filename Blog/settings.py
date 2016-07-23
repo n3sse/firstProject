5c2dd1e096af36df4397541c938c6267e6b6401e
szw <n3stis@hotmail.com>
2016-07-23 21:57:45 +0200

    1. Another day with 15-30minutes time. - I'm close to make layout, simple but for me enough. 2. Made: a) Bootstrap home page: - navbar - jumbotron - 3 thumbnails
    toDo:
    - footer for tommorow
    
    3. Need to add few views one for detail view and for blog view. I will make it tomorrow:
    - detail view will be simple: title, content, date + author.
    - blog view: 9 thumbnails for page + pagination.
    
    That's all my thoughts for now.

--- Blog/settings.py
+++ Blog/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'public/static')
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'public/static'),
 )
--- blog_page/views.py
+++ blog_page/views.py
@@ -9,4 +9,5 @@ class BlogPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(BlogPageView, self).get_context_data(**kwargs)
         context['recent_post'] = Post.objects.latest('post_date')
+        context['thumbnail_loop'] = range(1, 4)
         return context
