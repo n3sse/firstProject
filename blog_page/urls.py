e7b9c9d6383ce8c1349f567fdac534d8df3d56b4
szw <n3stis@hotmail.com>
2016-08-02 01:34:33 +0200

    Complete change of template concept, which I'll need to end tommorow.

--- blog_page/urls.py
+++ blog_page/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from .views import HomePageView, BlogPageView, PostDetailView
+
 urlpatterns = [
     url(r'^$', HomePageView.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', PostDetailView.as_view()),
     url(r'^blog/$', BlogPageView.as_view()),
+    url(r'^blog/(?P<pk>[0-9]+)/$', PostDetailView.as_view()),
+    url(r'^blog/(?P<page>\d+)/$', BlogPageView.as_view()),
 ]
--- blog_page/views.py
+++ blog_page/views.py
@@ -1,28 +1,32 @@
-from django.views.generic import TemplateView, DetailView
-from .models import Post
+from django.views.generic import TemplateView, DetailView, ListView
+from .models import Post, Author
 
 
 class HomePageView(TemplateView):
-
+    """ HomePageView as the name says,
+    it returns a view for index page.
+    It contains three newest Posts
+    and authors which are making this page."""
     template_name = "blog_page/index.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['recent_post'] = Post.objects.all()[:3]
-        # context['thumbnail_loop'] = range(1, 4)
+        context['authors'] = Author.objects.all()
         return context
 
 
-class BlogPageView(TemplateView):
-
+class BlogPageView(ListView):
+    """ BlogPageView returns a view for all
+        Posts which this site contains. """
+    model = Post
     template_name = "blog_page/blog_page.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(BlogPageView, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
-        return context
+    context_object_name = "post_list"
+    paginate_by = 6
 
 
 class PostDetailView(DetailView):
+    """ PostDetailView is a view which returns
+        full model of single Post. """
     model = Post
     template_name = "blog_page/detail_page.html"
